0f162c054095a0f53faea6276df1fd2e41486810
Rulee <rullyyustafnugroho@gmail.com>
2022-07-30 19:22:21 +0700

    api checkout and crud payments

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ const categoriesRouter = require('./app/api/v1/categories/router');
 const imagesRouter = require('./app/api/v1/images/router');
 const talentsRouter = require('./app/api/v1/talents/router');
 const eventsRouter = require('./app/api/v1/events/router');
+const organizersRouter = require('./app/api/v1/organizers/router');
+const authCMSRouter = require('./app/api/v1/auth/router');
+const ordersRouter = require('./app/api/v1/orders/router');
+const participantsRouter = require('./app/api/v1/participants/router');
+const paymentsRouter = require('./app/api/v1/payments/router');
 
-// middlewares
+const v1 = '/api/v1';
+
 const notFoundMiddleware = require('./app/middlewares/not-found');
 const handleErrorMiddleware = require('./app/middlewares/handler-error');
-
-const v1 = '/api/v1/cms';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,12 +33,16 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use(v1, categoriesRouter);
-app.use(v1, imagesRouter);
-app.use(v1, talentsRouter);
-app.use(v1, eventsRouter);
+app.use(`${v1}/cms`, categoriesRouter);
+app.use(`${v1}/cms`, imagesRouter);
+app.use(`${v1}/cms`, talentsRouter);
+app.use(`${v1}/cms`, eventsRouter);
+app.use(`${v1}/cms`, organizersRouter);
+app.use(`${v1}/cms`, authCMSRouter);
+app.use(`${v1}/cms`, ordersRouter);
+app.use(`${v1}/cms`, paymentsRouter);
+app.use(`${v1}`, participantsRouter);
 
-// middlewares
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddleware);
 
