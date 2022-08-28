9c3ea3676e5abb9bf2ae48105bbd77203adc95c8
Rulee <rullyyustafnugroho@gmail.com>
2022-08-28 21:05:05 +0700

    jwt refreshtoken

--- app/utils/jwt.js
+++ app/utils/jwt.js
@@ -1,31 +1,31 @@
 const jwt = require('jsonwebtoken');
 const {
-  jwtSecret,
-  jwtExpiration,
-  jwtRefreshTokenSecret,
-  jwtRefreshTokenExpiration,
+	jwtSecret,
+	jwtExpiration,
+	jwtRefreshTokenSecret,
+	jwtRefreshTokenExpiration,
 } = require('../config');
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, jwtSecret, {
-    expiresIn: jwtExpiration,
-  });
-  return token;
+	const token = jwt.sign(payload, jwtSecret, {
+		expiresIn: jwtExpiration,
+	});
+	return token;
 };
 const createRefreshJWT = ({ payload }) => {
-  const token = jwt.sign(payload, jwtRefreshTokenSecret, {
-    expiresIn: jwtRefreshTokenExpiration,
-  });
-  return token;
+	const token = jwt.sign(payload, jwtRefreshTokenSecret, {
+		expiresIn: jwtRefreshTokenExpiration,
+	});
+	return token;
 };
 
 const isTokenValid = ({ token }) => jwt.verify(token, jwtSecret);
 const isTokenValidRefreshToken = ({ token }) =>
-  jwt.verify(token, jwtRefreshTokenSecret);
+	jwt.verify(token, jwtRefreshTokenSecret);
 
 module.exports = {
-  createJWT,
-  isTokenValid,
-  createRefreshJWT,
-  isTokenValidRefreshToken,
+	createJWT,
+	isTokenValid,
+	createRefreshJWT,
+	isTokenValidRefreshToken,
 };
